b494cc296022f44939ea791f7c6b3da773074cc6
shaik aqib <ragesh.1995@gmail.com>
2018-03-06 17:45:16 +0530

    pods added

--- RakPractice/RakPractice/ViewController.swift
+++ RakPractice/RakPractice/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SystemConfiguration
 
 class ViewController: UIViewController {
 
@@ -67,6 +68,9 @@ class ViewController: UIViewController {
     }
     
     @objc func getInfo(x: Int) {
+        if isInternetAvailable() == false {
+            return
+        }
         let url = "http://numbersapi.com/\(x)";
         let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
         let urls = URL(string: url)!
@@ -111,6 +115,27 @@ class ViewController: UIViewController {
         }, completion: {(isCompleted) in
             toastLabel.removeFromSuperview()
         })
+    }
+    
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
     }
 
 
